3e460163a292397889d0c1f8971a62602631da56
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-10-08 18:42:28 +0500

    data converted into array and stored as var

--- js-task/my-app/src/Components/Data.js
+++ js-task/my-app/src/Components/Data.js
@@ -10,7 +10,26 @@ function Data() {
     </div>
   );
 }
-const result = Object.values(content.para);
-console.log(result);
+const result = Object.values(content.para.split(' '));
+// console.log(result);
+
+let keywords = content.keywords;
+// console.log(keywords)
+
+const intersection = result.filter(element => keywords.includes(element));
+// console.log(intersection)
+
+
+
+
+// Create sample arrays variables.
+// const domainsArr1 = ["infinitbility", "aGuideHub", "SortoutCode"];
+// const domainsArr2 = ["notebility", "aGuideHub", "repairbility"];
+// To get common elements in new array use `filter()` method
+// Use `includes()` method to check string is available in array
+const filteredArray = keywords.filter(value => result.includes(value));
+console.log("filteredArray", filteredArray)
+// Expected 'filteredArray' [ 'aGuideHub' ]
+
 
 export default Data;
